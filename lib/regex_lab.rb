cb46719fdce58aaa625f26397fcd365525993783
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-27 14:20:22 +0300

    commit tree f98a6b062c5c520f0d21e52a2f432741e96cd99d

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,15 @@
 def starts_with_a_vowel?(word)
-
+    word.start_with?(/[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    # text.grep(//)
+    words = text.start_with?(/un/i) && text.end_with?('ing')
+        
 end
 
 def words_five_letters_long(text)
-
+    text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
